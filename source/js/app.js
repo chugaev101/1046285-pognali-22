6f020fd764c2c580ef5037165b2ad0e177955905
chugaev101 <chugaev101@gmail.com>
2021-05-21 20:04:40 +0300

    Adds scripts to form.html.

--- source/js/app.js
+++ source/js/app.js
@@ -1,4 +1,5 @@
 let header = document.querySelector(".page-header");
+let headerContainer = document.querySelector(".page-main__title-container");
 let headerLogo = document.querySelector(".page-header__logo");
 let headerMenu = document.querySelector(".page-header__menu");
 let headerMenuButton = document.querySelector(".page-header__menu-toggle");
@@ -8,27 +9,96 @@ let ratesModal = document.querySelector(".rates__modal");
 let ratesModalButton = document.querySelector(".rates__modal-button");
 let feedbackForm = document.querySelector(".feedback__registration-form");
 let feedbackFormInput = document.querySelector(".feedback__registration-form-email");
+let addPlan = document.querySelector(".add-plan");
+let addPlanPaginationPoints = document.querySelectorAll(".add-plan__pagination-item");
+let addPlanSteps = document.querySelectorAll(".add-plan__step");
+let addPlanButtonsForward = document.querySelectorAll(".add-plan__button");
+let addPlanButtonsBack = document.querySelectorAll(".add-plan__previous-step-button");
+
+// plan step toggle
+
+for (let i = 0; i < addPlanButtonsForward.length; i++) {
+  let button = addPlanButtonsForward[i];
+  button.addEventListener("click", clickNextStep)
+}
+
+for (let i = 0; i < addPlanButtonsBack.length; i++) {
+  let buttonBack = addPlanButtonsBack[i];
+  buttonBack.addEventListener("click", clickPreviousStep);
+}
+
+function clickNextStep(evt) {
+  let currentIndex;
+
+  for (let i = 0; i < addPlanSteps.length - 1; i++) {
+    if (addPlanSteps[i].classList.contains("add-plan__step--current")) {
+      currentIndex = i;
+    }
+  }
+
+  if (currentIndex < addPlanSteps.length - 1) {
+    evt.preventDefault();
+    addPlanSteps[currentIndex].classList.remove("add-plan__step--current");
+    addPlanSteps[currentIndex + 1].classList.add("add-plan__step--current");
+
+    addPlanPaginationPoints[currentIndex].classList.remove("add-plan__pagination-item--current");
+    addPlanPaginationPoints[currentIndex + 1].classList.add("add-plan__pagination-item--current");
+  }
+
+  currentIndex = "";
+}
+
+function clickPreviousStep(evt) {
+  evt.preventDefault();
+
+  for (let i = 0; i < addPlanSteps.length; i++) {
+    if (addPlanSteps[i].classList.contains("add-plan__step--current")) {
+
+      addPlanSteps[i].classList.remove("add-plan__step--current");
+      addPlanSteps[i - 1].classList.add("add-plan__step--current");
+
+      addPlanPaginationPoints[i].classList.remove("add-plan__pagination-item--current");
+      addPlanPaginationPoints[i - 1].classList.add("add-plan__pagination-item--current");
+    }
+  }
+
+  currentIndex = "";
+}
 
 // no script deleted
 
 window.onload = function() {
   header.classList.remove("page-header--no-script");
+  addPlan.classList.remove("add-plan--no-script");
+
+  if (promoBlock) {
+    promoBlock.classList.remove("promo--no-script");
+  }
+
+  if (headerContainer) {
+    headerContainer.classList.remove("page-main__title-container--no-script");
+  }
 }
 
-// header after scroll
+// after scroll
 
-window.addEventListener("scroll", function() {
+const afterScroll = function(block, modifier) {
+  window.addEventListener("scroll", function() {
 
-  if (pageYOffset > 0) {
-    header.classList.add("page-header--after-scroll");
-    headerLogo.classList.add("page-header__logo--after-scroll");
-    promoBlock.classList.add("promo--after-scroll");
-  } else {
-    header.classList.remove("page-header--after-scroll");
-    headerLogo.classList.remove("page-header__logo--after-scroll");
-    promoBlock.classList.remove("promo--after-scroll");
-  }
-});
+    if (pageYOffset > 0) {
+      block.classList.add(modifier);
+    } else {
+      block.classList.remove(modifierr);
+    }
+  });
+}
+
+afterScroll(header, "page-header--after-scroll")
+afterScroll(headerLogo, "page-header__logo--after-scroll")
+
+if (promoBlock) {
+  afterScroll(promoBlock, "promo--after-scroll")
+}
 
 // header menu toggle
 
@@ -39,31 +109,35 @@ headerMenuButton.addEventListener("click", function() {
 
 // rates modal toggle
 
-ratesButton.addEventListener("click", function(evt) {
-  evt.preventDefault();
-  ratesModal.classList.add("rates__modal--opened");
-});
+if (ratesModal) {
+  ratesButton.addEventListener("click", function(evt) {
+    evt.preventDefault();
+    ratesModal.classList.add("rates__modal--opened");
+  });
 
-ratesModalButton.addEventListener("click", function() {
-  ratesModal.classList.remove("rates__modal--opened");
-});
+  ratesModalButton.addEventListener("click", function() {
+    ratesModal.classList.remove("rates__modal--opened");
+  });
+}
 
 // input email validation
 
-feedbackForm.addEventListener("submit", function(evt) {
-  if (!feedbackFormInput.value) {
-    evt.preventDefault();
+if (feedbackForm) {
+  feedbackForm.addEventListener("submit", function(evt) {
+    if (!feedbackFormInput.value) {
+      evt.preventDefault();
 
-    let i = 0;
+      let i = 0;
 
-    let error = setInterval(function() {
-      i++;
+      let error = setInterval(function() {
+        i++;
 
-      feedbackFormInput.classList.toggle("feedback__registration-form-email--error");
+        feedbackFormInput.classList.toggle("feedback__registration-form-email--error");
 
-      if (i === 6) {
-        clearInterval(error);
-      }
-    }, 200);
-  }
-});
+        if (i === 6) {
+          clearInterval(error);
+        }
+      }, 200);
+    }
+  });
+}
